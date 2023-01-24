acb111cbdee307acdf7a6f054253d62cd3de4865
Ariana Miranda <ariichazkel@gmail.com>
2023-01-23 21:27:16 -0300

    operação matematica basica

---
+++ javaScript/secao-3.1-introducao-javaScript/dia-01-primeiros-passos/scripts.js
@@ -0,0 +1,14 @@
+const num1 = 10;
+const num2 = 5;
+
+let sum = num1 + num2;
+let subtracao = num1 - num2;
+let multiplicacao = num1 * num2;
+let divisao = num1 / num2;
+let resto = num1 % num2;
+
+console.log(sum);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+console.log(resto);

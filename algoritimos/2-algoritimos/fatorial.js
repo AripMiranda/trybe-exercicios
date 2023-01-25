88ccfa4e2e8263429219d4c082dc16a6c8c8dcbc
Ariana Miranda <ariichazkel@gmail.com>
2023-01-25 19:28:09 -0300

    fatorial

---
+++ algoritimos/2-algoritimos/fatorial.js
@@ -0,0 +1,13 @@
+//let fatorial = 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1 ;
+//console.log(fatorial);
+let n = 10;
+let resultado = 1;
+
+for(let i = n; i > 0; i--){
+    resultado *= i
+} 
+console.log(resultado);
+
+
+
+
